8f5c0ca6ad395bd317bec0991d7ec3131e326ac2
Amanda Basso de Oliveira <aoliveira@localhost.localdomain>
2020-11-30 21:08:35 -0300

    Initial commit

---
+++ IRC/autograde.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import os
+import signal
+import json
+import subprocess
+import time
+
+
+def main():
+    tests_weight = 10*[1]
+    scores = {}
+
+    if os.path.exists('./compilar'):
+        print('Chamando ./compilar')
+        os.system('./compilar')
+
+    for i, weight in enumerate(tests_weight):
+        testno = i + 1
+        pid = os.spawnlp(os.P_NOWAIT, './servidor', './servidor')
+        print('\nServidor executando no pid=%d' % pid)
+        time.sleep(.5)
+
+        test = 'test%d' % testno
+        scores[test] = 0
+
+        print('Teste #%d' % testno)
+        timeout = 5
+        p = subprocess.Popen('./%s.py' % test)
+        try:
+            if p.wait(timeout=timeout) == 0:
+                scores[test] = weight
+                print('OK')
+        except subprocess.TimeoutExpired:
+            print('%s: TIMEOUT (%.3f s)' % (test, timeout))
+            p.kill()
+
+        os.kill(pid, signal.SIGKILL)
+        os.waitpid(pid, 0)
+
+    print(json.dumps({'scores':scores}))
+
+
+if __name__ == '__main__':
+    main()
