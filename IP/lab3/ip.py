b603327706ef70eb7ff6a9d17c5a915382eea7e7
Amanda Basso de Oliveira <amanda.basso.oliveira@gmail.com>
2021-01-09 18:16:35 -0300

    Implement third step

--- IP/lab3/ip.py
+++ IP/lab3/ip.py
@@ -18,6 +18,12 @@ class IP:
         self.tabela_encaminhamento = []
         # Lista de cidrs que incluem o destino, para selecionar aquele que é mais restrito
         self.lista_cidr = []
+        # Cidr mais restritivo
+        self.cidr_restritivo = None
+        # next hop associado ao cidr mais restritivo
+        self.next_hop = None
+        # Flag primeira iteracao
+        self.flag = True
 
     def __raw_recv(self, datagrama):
         dscp, ecn, identification, flags, frag_offset, ttl, proto, \
@@ -32,12 +38,32 @@ class IP:
             # TODO: Trate corretamente o campo TTL do datagrama
             self.enlace.enviar(datagrama, next_hop)
 
-    def _busca_addr_em_cidr(self, addr, cidr):
-        range_cidr = ip_network(cidr)
-        print(range_cidr)
-        return ip_address(addr) in range_cidr
+    def _busca_addr_em_cidr(self, addr, cidr, next_hop):
+        if ip_address(addr) in ip_network(cidr):
+            # Na primeira iteração
+            if self.flag:
+                print("1 Primeira iteracao")
+                self.cidr_restritivo = cidr
+                print(f"1 {self.cidr_restritivo}")
+                self.next_hop = next_hop
+                print(f"1 {self.next_hop}")
+                self.flag = False
+            else:
+                print("2 Segunda iteracao")
+                print(f"2 {self.cidr_restritivo}")
+                print(f"2 {self.next_hop}")
+                n_atual = cidr.split('/')[1]
+                n_cidr_restritivo = self.cidr_restritivo.split('/')[1]
+                if int(n_atual) > int(n_cidr_restritivo):
+                    print(f"3 {n_atual}")
+                    print(f"3 {n_cidr_restritivo}")
+                    print("3 Atualiza")
+                    self.cidr_restritivo = cidr
+                    print(f"3 {self.cidr_restritivo}")
+                    self.next_hop = next_hop
+                    print(f"3 {self.next_hop}")
 
-    def _cidr_mais_restritivo(self):
+
 
     def _next_hop(self, dest_addr):
         # TODO: Use a tabela de encaminhamento para determinar o próximo salto
@@ -45,12 +71,18 @@ class IP:
         # Retorne o next_hop para o dest_addr fornecido.
         # Passo 1: aqui, o next_hop só é considerado se o range do cidr incluir o destino
         for hop in self.tabela_encaminhamento:
-            # Se não estiver na linha procurada, continua procurando
-            if not self._busca_addr_em_cidr(dest_addr, hop[0]):
-                continue
-            else:
-                 self.lista_cidr.append(hop[1]) # Se encontrar, retorna o next_hop para o dest_addr fornecido
-        return None # Se não encontrou nada, retorna None
+            self._busca_addr_em_cidr(dest_addr, hop[0], hop[1])
+
+        if self.next_hop != "":
+            next_hop = self.next_hop
+            self.next_hop = None
+            self.cidr_restritivo = None
+            self.flag = True
+            print(f"4 {next_hop}")
+            return next_hop # Retorna cidr mais restritivo
+        else:
+            return None # Se não encontrou nada, retorna None
+
         pass
 
     def definir_endereco_host(self, meu_endereco):
